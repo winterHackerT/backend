87ebde2f9fa5f4c93d9dea51489f25b96c3a79b9
YeJun, Jung <yejun614@naver.com>
2023-01-05 04:26:02 +0900

    Feat: add passwordEncoder

--- src/main/java/com/winterhack/wiki/Configuration/SecurityConfiguration.java
+++ src/main/java/com/winterhack/wiki/Configuration/SecurityConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -32,6 +34,11 @@ public class SecurityConfiguration {
     http.csrf().disable();
 
     return http.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 
 }
