9b87c005a9e6b220a4c309a590d674c4533055f2
YeJun, Jung <yejun614@naver.com>
2023-01-06 03:55:59 +0900

    Fix: method name

--- src/main/java/com/winterhack/wiki/Controller/DocumentController.java
+++ src/main/java/com/winterhack/wiki/Controller/DocumentController.java
@@ -1,23 +1,28 @@
 package com.winterhack.wiki.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.winterhack.wiki.Data.ResultDTO;
+import com.winterhack.wiki.Service.DocumentService;
 
 
 @RestController
 public class DocumentController {
 
+  @Autowired
+  private DocumentService documentService;
+
   @RequestMapping(method = RequestMethod.POST, path = "/docs")
-  public ResultDTO postDocument() {}
+  public ResultDTO post() {}
 
   @RequestMapping(method = RequestMethod.GET, path = "/docs/{documentTitle}")
-  public ResultDTO getDocument(@PathVariable("documentTitle") String documentTitle) {}
+  public ResultDTO read(@PathVariable("documentTitle") String documentTitle) {}
 
   @RequestMapping(method = RequestMethod.DELETE, path = "/docs/{documentTitle}")
-  public ResultDTO deleteDocument(@PathVariable("documentTitle") String documentTitle) {}
+  public ResultDTO delete(@PathVariable("documentTitle") String documentTitle) {}
 
 }
