a5452393b2085c86b420b404c44199a32b06f283
YeJun, Jung <yejun614@naver.com>
2023-01-06 15:33:44 +0900

    Fix: working column for controller and service

--- src/main/java/com/winterhack/wiki/Controller/DocumentController.java
+++ src/main/java/com/winterhack/wiki/Controller/DocumentController.java
@@ -1,6 +1,7 @@
 package com.winterhack.wiki.Controller;
 
 import java.security.Principal;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -10,10 +11,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.winterhack.wiki.Data.Document.PostDocumentDTO;
 import com.winterhack.wiki.Data.Document.ReadDocumentDTO;
+import com.winterhack.wiki.Data.Document.ReadDocumentHistoryDTO;
 import com.winterhack.wiki.Data.User.ResultDTO;
 import com.winterhack.wiki.Entity.DocumentEntity;
 import com.winterhack.wiki.Entity.UserEntity;
@@ -33,6 +36,15 @@ public class DocumentController {
 
   @RequestMapping(method = RequestMethod.POST, path = "/docs")
   public ResultDTO post(HttpServletRequest request, Principal principal, @RequestBody @Valid PostDocumentDTO document) {
+    String working = "문서 수정";
+    DocumentEntity documentEntity = documentService.readDocument(document.getTitle());
+
+    if (documentEntity == null) {
+      working = "새로운 문서 추가";
+    } else if (documentEntity.getContent().equals("")) {
+      working = "삭제된 문서 복구";
+    }
+
     UserEntity user = null;
 
     if (principal != null) {
@@ -49,6 +61,7 @@ public class DocumentController {
       document.getContent(),
       user,
       request.getRemoteAddr(),
+      working,
       document.getMessage()
     );
 
@@ -80,7 +93,12 @@ public class DocumentController {
   }
 
   @RequestMapping(method = RequestMethod.DELETE, path = "/docs/{documentTitle}")
-  public ResultDTO delete(HttpServletRequest request, Principal principal, @PathVariable("documentTitle") String documentTitle) {
+  public ResultDTO delete(
+    HttpServletRequest request,
+    Principal principal,
+    @PathVariable("documentTitle") String documentTitle,
+    @RequestParam("message") String message
+  ) {
     DocumentEntity documentEntity = documentService.readDocument(documentTitle);
 
     if (documentEntity == null) {
@@ -103,7 +121,8 @@ public class DocumentController {
       "",
       user,
       request.getRemoteAddr(),
-      "문서 삭제"
+      "문서 삭제",
+      message
     );
 
     return new ResultDTO("문서 삭제", true);
--- src/main/java/com/winterhack/wiki/Service/DocumentService.java
+++ src/main/java/com/winterhack/wiki/Service/DocumentService.java
@@ -2,10 +2,13 @@ package com.winterhack.wiki.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.winterhack.wiki.Data.Document.ReadDocumentHistoryDTO;
 import com.winterhack.wiki.Entity.DocumentEntity;
 import com.winterhack.wiki.Entity.UserEntity;
 import com.winterhack.wiki.Repository.DocumentRepository;
@@ -16,13 +19,14 @@ public class DocumentService {
   @Autowired
   private DocumentRepository documentRepository;
 
-  public void postDocument(String title, String content, UserEntity user, String addr, String message) {
+  public void postDocument(String title, String content, UserEntity user, String addr, String working, String message) {
     DocumentEntity documentEntity = new DocumentEntity();
 
     documentEntity.setTitle(title);
     documentEntity.setContent(content);
     documentEntity.setUser(user);
     documentEntity.setAddr(addr);
+    documentEntity.setWorking(working);
     documentEntity.setMessage(message);
     documentEntity.setDatetime(LocalDateTime.now());
 
