18e813b1b2889701a5c2c126ae5c362faa2b63cc
YeJun, Jung <yejun614@naver.com>
2023-01-06 17:26:44 +0900

    Refactor: remove unused imports

--- src/main/java/com/winterhack/wiki/Controller/DocumentController.java
+++ src/main/java/com/winterhack/wiki/Controller/DocumentController.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
