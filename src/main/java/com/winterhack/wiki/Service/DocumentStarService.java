b046f00f13ce6c6a7b88254e23d843e1e011ebb9
YeJun, Jung <yejun614@naver.com>
2023-01-07 02:04:07 +0900

    Refactor: remove unused imports

--- src/main/java/com/winterhack/wiki/Service/DocumentStarService.java
+++ src/main/java/com/winterhack/wiki/Service/DocumentStarService.java
@@ -4,9 +4,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
